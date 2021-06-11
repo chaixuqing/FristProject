55773001deac124becbce6a580fe228af809bb4f
xuqingchai <xuqingchai00@gmail.com>
2021-06-11 19:42:26 +0800

    dev commit

--- src/include/primer/p0_starter.h
+++ src/include/primer/p0_starter.h
@@ -9,7 +9,6 @@
 // Copyright (c) 2015-2020, Carnegie Mellon University Database Group
 //
 //===----------------------------------------------------------------------===//
-// modifyeds
 #pragma once
 
 #include <memory>
